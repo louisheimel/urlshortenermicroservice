c85930e66e373225dd8e4142963751044627d357
Unknown Name <louis.heimel@mines.sdsmt.edu>
2017-01-10 20:54:47 +0000

    get index page running

--- server.js
+++ server.js
@@ -1,3 +1,9 @@
 var express = require('express');
+var app = express();
+var path = require('path');
+
+app.use('/', express.static(path.join(__dirname, 'public')));
+
+app.get('/', function(req, res) { res.redirect('/static/index.html'); });
 
 app.listen(8080);
